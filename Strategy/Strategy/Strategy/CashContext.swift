6c6cd16db832b66e43288468e1e4f1a5a1a5cc44
qingfeng <qingfengiosdev@gmail.com>
2018-07-09 14:35:19 +0800

    update Strategy

--- Strategy/Strategy/Strategy/CashContext.swift
+++ Strategy/Strategy/Strategy/CashContext.swift
@@ -11,7 +11,9 @@ import UIKit
 
 /// 策略模式
 /**
+ 策略模式是一种定义一系列算法的方法，从概念上来看，所有这些算法完成的都是相同的工作，只是实现不同，它可以以相同的方式调用所有的算法，减少了各种算法类与使用算法的耦合
  
+ 策略模式的Strategy类层次为Context定义可一系列可供重用的算法或行为。继承有助于析取出这些算法的公共功能
  */
 class CashContext: NSObject {
 
@@ -29,6 +31,11 @@ class CashContext: NSObject {
         }
     }
     
+    
+    /// 获取最终结果
+    ///
+    /// - Parameter fee: 源费用
+    /// - Returns: 结果费用
     func getResult(fee: Double) -> Double {
         return cs.acceptCash(originFee: fee)
     }
